47d5532d55eb1f26258ee2f87860512c2bd1cca4
aoineco <masaya891047@gmail.com>
2022-08-11 20:14:03 +0900

    first commit

---
+++ .history/sql_20220811200339.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDBPDO";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+
+    $conn->exec($sql);
+    echo "New record created successfully";
+} catch(PDOException $e) {
+    echo` $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
