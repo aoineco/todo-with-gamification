6152f0f1f5e8c01a36d61ba0837cec772300e074
aoineco <masaya891047@gmail.com>
2022-09-09 15:34:48 +0900

    slot game

--- .history/random_reward/index_20220909145034.php
+++ .history/random_reward/index_20220909145034.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
     <?php
 
-    function random_number()
-    {
-        $a = rand(1, 6);
-        $b = rand(1, 6);
-        echo $a . '  ' . $b . '<br>';
+    function random_number(){
+        $a = rand(1,6);
+        $b = rand(1,6);
+        echo $a.'  '. $b.'<br>';
         if ($a == $b) {
             echo 'congratulations!!';
-        } else {
+        }else{
             echo 'Too bad. Try again.';
         }
     }
@@ -32,7 +29,5 @@
         <input type="submit" value="try" name="try">
     </form>
 
-    <a href="http://localhost:8888/gamification/random_reward/lab.html">スロットゲーム</a>
 </body>
-
 </html>
